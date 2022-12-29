fa24ea74310b6ce344505f8572a5304ca3f47b22
enrique-lol <enrique.memije.pro@gmail.com>
2022-12-28 19:31:39 -0600

    some slight

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React, { Component, Fragment } from 'react'
 import { Route } from 'react-router-dom'
 import { v4 as uuid } from 'uuid'
 
-import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
 import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
 import Header from './components/Header/Header'
-import SignUp from './components/SignUp/SignUp'
-import SignIn from './components/SignIn/SignIn'
-import SignOut from './components/SignOut/SignOut'
-import ChangePassword from './components/ChangePassword/ChangePassword'
+import IndexArticles from './routes/IndexArticles.js'
+import NewArticle from './routes/NewArticle.js'
 
 class App extends Component {
   constructor (props) {
@@ -53,17 +50,11 @@ class App extends Component {
           />
         ))}
         <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
+          <Route exact path='/' render={() => (
+            <IndexArticles msgAlert={this.msgAlert} />
           )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword msgAlert={this.msgAlert} user={user} />
+          <Route exact path='/new-article' render={() => (
+            <NewArticle msgAlert={this.msgAlert} />
           )} />
         </main>
       </Fragment>
@@ -71,4 +62,22 @@ class App extends Component {
   }
 }
 
+// AUTH
+// import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
+// import SignUp from './components/SignUp/SignUp'
+// import SignIn from './components/SignIn/SignIn'
+// import SignOut from './components/SignOut/SignOut'
+// import ChangePassword from './components/ChangePassword/ChangePassword'
+// <Route path='/sign-up' render={() => (
+//   <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
+// )} />
+// <Route path='/sign-in' render={() => (
+//   <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
+// )} />
+// <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+//   <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
+// )} />
+// <AuthenticatedRoute user={user} path='/change-password' render={() => (
+//   <ChangePassword msgAlert={this.msgAlert} user={user} />
+// )} />
 export default App
